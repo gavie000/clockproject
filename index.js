e0a87f24deffa53ce1d5f336c7fbad582cffbc10
gavie000 <espinoza.software.3000@gmail.com>
2022-11-15 06:11:38 -0800

    working on seconds functions

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-setInterval(setClock, 1000)
+//setInterval(setClock, 1000)
 
-const hourHand = document.querySelector('[data-hour-hand]')
-const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-second-hand]')
+//not to self. work on the second hand function it is not working 
+
+
+
+
+//const hourHand = document.querySelector('[data-hour-hand]');
+//const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector(".data-second-hand");
 
 function setClock() {
-   const currentDate = new Date()
-   const secondsRatio = currentDate.getSeconds() / 60
-   const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-   const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
-   setRotation(secondHand, secondsRatio)
-   setRotation(minuteHand, minutesRatio)
-   setRotation(hourHand, hoursRatio)
+   const currentDate = new Date();
+
+   //seconds function
+   //const secondsRatio = currentDate.getSeconds() / 60
+   const seconds = currentDate.getSeconds();
+   const secondsRatio = ((seconds / 60) * 360) + 90;
+   secondHand.style.transform = `rotate(${secondsRatio}deg)`;
+
+   //minute function
+   //const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+   
+   //hour function
+   //const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+   
+   //original code below
+  // setRotation(secondHand, secondsRatio)
+   //setRotation(minuteHand, minutesRatio)
+   //setRotation(hourHand, hoursRatio)
 
 }
 
-function setRotation(element, rotationRatio) {
-   element.style.setProperty('--rotation', rotationRatio * 360)
-}
+//function setRotation(element, rotationRatio) {
+  // element.style.setProperty('--rotation', rotationRatio * 360)
+//}
 
+setInterval(setClock, 1000);
 
-setClock()
+setClock();
